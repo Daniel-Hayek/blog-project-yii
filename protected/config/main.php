728b339123da881a43dddd3b576bdd0e0c259cd7
Danny <dhayek92@gmail.com>
2025-12-13 16:48:20 +0200

    Added link from home page to go to posts page and removed some of the generated elements

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'A Blog Website',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,15 +6,8 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>This is my personal blog, where I post my thoughts and allow others to read them!</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<p>Here you may see all the posts, or create some of your own!</p>
 
-<p>For more details on how to further develop this application, please read
-the <a href="https://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="https://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<a href="http://localhost/blog-project-yii/index.php?r=post/index">Posts</a>
